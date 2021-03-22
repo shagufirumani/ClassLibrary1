94442b32ee2ec46709f0be1cb0e74d616354f525
shagufirumani <rshagufi@gmail.com>
2021-03-22 15:15:42 +0530

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void main()
+        {
+            a a1 = new a();
+
+           a1. show(10, 20);
+        }
+
+       
+    }
+    public class a
+    {
+
+        public void show(int a, int b)
+        {
+            Console.WriteLine("{0}{1}", a, b);
+        }
+
+    }
+}
